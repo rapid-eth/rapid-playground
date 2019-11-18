32e085f0bfc4729e675b1bdff2ffd6db0aa95bb5
Luis Osta <luisosta1999@gmail.com>
2019-11-17 20:51:54 -0600

    cleaned up repo

--- packages/apps/ethers-demo/src/pages/Home/index.jsx
+++ packages/apps/ethers-demo/src/pages/Home/index.jsx
@@ -3,13 +3,8 @@ import {withEthers} from 'ethers-react-system';
 import TitleNumber from './TitleNumber.js';
 
 /* --- Component --- */
-/**
- * @todo Make the initialization of the address and wallet be syncronous if the ethereum has already been enabled.
- */
+
 class Home extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     const {ethers} = this.props;
     console.log(ethers);
